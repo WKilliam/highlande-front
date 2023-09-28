cf274d4c17c455c22411eccc24181ef0d8c712b4
BearmanAdmin <william.k97430@gmail.com>
2023-09-28 17:36:27 +0200

    config jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,8 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { paths } = require('./tsconfig.json').compilerOptions;
+
+// jest.config.js (javascript file)
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  moduleNameMapper: {
-    '^@angular/core$': '<rootDir>/node_modules/@angular/core',
-    '^@angular/common$': '<rootDir>/node_modules/@angular/common',
-    '^@angular/forms$': '<rootDir>/node_modules/@angular/forms',
-    '^@angular/router$': '<rootDir>/node_modules/@angular/router',
-    '^@angular/platform-browser$': '<rootDir>/node_modules/@angular/platform-browser',
-    '^@angular/platform-browser-dynamic$': '<rootDir>/node_modules/@angular/platform-browser-dynamic',
-  },
-  transformIgnorePatterns: ['node_modules/(?!@ngrx|ngx-socket-io)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
