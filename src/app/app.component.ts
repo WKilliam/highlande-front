444a13d3ba6a205b11f7c324ee4d743804745cd2
BearmanAdmin <william.k97430@gmail.com>
2023-09-29 11:31:13 +0200

    websocket links OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,52 @@
-import {Component} from '@angular/core';
-import {NgSwitch, NgSwitchDefault, NgSwitchCase} from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {NgSwitch, NgSwitchDefault, NgSwitchCase, NgForOf} from '@angular/common';
 import {io} from 'socket.io-client';
 import {FormsModule} from "@angular/forms";
+import {ChatService} from "./web.io";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, FormsModule]
+  imports: [
+    NgSwitch,
+    NgSwitchDefault,
+    NgSwitchCase,
+    FormsModule,
+    NgForOf,
+  ]
 })
 export class AppComponent {
   title = 'highlande-front';
-  message: string = '';
-  socket = io('http://localhost:3000/',{ transports : ['websocket'] });
-  serverMessages: string[] = [];
+  newMessage: string ='';
+  messageList: string[] = [];
+  readonly #chatService: ChatService = inject(ChatService);
 
-  constructor() {
-    // Écoutez les messages du serveur
-    this.socket.on('messageFromServer', (data) => {
-      console.log('Message from server:', data);
-      // Ajoutez le message au tableau des messages du serveur
-      this.serverMessages.push(data);
-    });
+  ngOnInit() {
+    this.#chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
   }
 
-  // Fonction pour envoyer un message au serveur
-  sendMessageToServer() {
-    this.socket.emit('messageFromClient', this.message);
-    this.message = ''; // Effacez le champ de saisie après l'envoi
+  sendMessage() {
+    this.#chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
   }
+
+  // message: string = '';
+  // socket = io('http://localhost:3000',{ transports : ['websocket'] });
+  // serverMessages: string[] = [];
+  //
+  // // Fonction pour envoyer un message au serveur
+  // sendMessageToServer() {
+  //   // Écoutez les messages du serveur
+  //   this.socket.on('messageFromServer', (data) => {
+  //     console.log('Message from server:', data);
+  //     // Ajoutez le message au tableau des messages du serveur
+  //     this.serverMessages.push(data);
+  //   });
+  //   this.socket.emit('messageFromClient', this.message);
+  //   this.message = ''; // Effacez le champ de saisie après l'envoi
+  // }
 }
